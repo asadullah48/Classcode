ee65800254ed0616ac5b0753e5b7df130f11a38d
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-03-10 11:16:17 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//strong typed syntax
+var a = "Pakistan";
+var b = 9;
+var c = true;
+//type inference
+var e = "USA";
+var f = 10.9;
+var g = false;
---
+++ main.ts
@@ -0,0 +1,10 @@
+//strong typed syntax
+let a : string = "Pakistan";
+let b : number = 9;
+let c : boolean = true;
+
+//type inference
+let e = "USA";
+let f = 10.9;
+let g = false;
+console.log(a + b + c);
